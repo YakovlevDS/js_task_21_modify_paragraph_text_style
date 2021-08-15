1aadf4a5ca7138cb1cf1d69ddb3557b418b69cfd
Dmitriy Yakovlev <1d3ddd@gmail.com>
2021-08-15 17:59:20 +0300

    added second p

---
+++ app.js
@@ -0,0 +1,20 @@
+// ? Task:  JavaScript: Modify paragraph text style through javascript code using button
+
+// Solution 1
+const js_style=()=> 
+{
+//font styles added by JS:
+ text.style.fontSize = "14px";
+ text.style.fontFamily = "Comic Sans MS";
+ text.style.color = "green";
+}
+const js_style1=()=> 
+{
+//font styles added by JS:
+ text1.style.fontSize = "20px";
+ text1.style.fontFamily = "roboto";
+ text1.style.color = "red";
+}
+
+
+// ! Explanation:
